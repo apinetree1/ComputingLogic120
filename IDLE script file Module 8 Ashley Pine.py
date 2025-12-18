cc906ed7a78867b66ebcba59adc8161e42b2834f
apinetree1 <apine1206@gmail.com>
2025-12-17 21:33:29 -0500

    Add files via upload

---
+++ IDLE script file Module 8 Ashley Pine.py
@@ -0,0 +1,11 @@
+def greaterthan(x,y):
+    if x > y:
+        return True
+    else:
+        return False
+
+
+a = 10
+b = 6
+result = greaterthan(a,b)
+print ("The statement ten is greater than six is " + str(result))
